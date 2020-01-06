7a4b51fa12bca15ff48ad7c141be7cbcc432b08f
Thibault Balsamo <balsamothibault@hotmail.fr>
2020-01-06 10:38:29 +0100

    changement des icones : done

--- Weather/Code/View/HomeViewController/Cells/HomeCell.swift
+++ Weather/Code/View/HomeViewController/Cells/HomeCell.swift
@@ -23,7 +23,44 @@ class HomeCell: UICollectionViewCell {
     //MARK: Configure Cell
     func configurewith(_ weather: WeatherProtocol, at index: Int) {
         self.backgroundColor = WeatherUtils.weatherColor(index: index)
+        self.selectImage(weather)
         self.dateLabel.text = WeatherUtils.changeDateFormat(dateString: weather.date) 
         self.temperatureLabel.text = String(format: "%.0f", weather.temperature! - 273.15) + "°C"
+        //self.weatherImageView
+    }
+    
+    func selectImage(_ weather: WeatherProtocol) {
+        
+        guard let humidity = weather.humidity else {
+            return
+        }
+        
+        guard let wind = weather.wind else {
+            return
+        }
+        
+        guard let rain = weather.rain else {
+            return
+        }
+        
+        if (rain >= 0.3 && wind < 21.0) {
+            self.weatherImageView.image = UIImage(named: "rain_sky")
+        }
+        
+        if (rain < 0.3 && humidity >= 60.0) {
+            self.weatherImageView.image = UIImage(named: "foggy_sky")
+        }
+        
+        if (rain >= 0.3 && wind > 21.0) {
+            self.weatherImageView.image = UIImage(named: "wind_sky")
+        }
+        
+        if (wind < 21.0 && rain < 0.3) {
+            self.weatherImageView.image = UIImage(named: "clear_sky")
+        }
+        
+        if (wind > 21.0 && rain > 0.3) {
+            self.weatherImageView.image = UIImage(named: "wind_rain_sky")
+        }
     }
 }
